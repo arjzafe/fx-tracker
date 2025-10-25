2e660d45446df82570f59fff4cd9856c3b2d37d2
Arriane Zafe <arriane.zafe@forgeahedge.com>
2025-10-25 13:52:08 +0800

    Add CurrencySelector unit test and add currency mock data

---
+++ src/__mocks__/mockCurrencyData.ts
@@ -0,0 +1,10 @@
+export const mockCurrencyData = {
+  gbp: "British Pound",
+  usd: "United States Dollar",
+  eur: "Euro",
+  jpy: "Japanese Yen",
+  chf: "Swiss Franc",
+  cad: "Canadian Dollar",
+  aud: "Australian Dollar",
+  zar: "South African Rand",
+};
---
+++ src/components/CurrencySelector.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { CurrencySelector } from "./CurrencySelector";
+import { mockCurrencyData } from "../__mocks__/mockCurrencyData";
+
+describe("CurrencySelector", () => {
+  const mockOnCurrencyChange = vi.fn();
+  const defaultSelectedCurrency = "gbp";
+
+  beforeEach(() => {
+    render(
+      <CurrencySelector
+        currencies={mockCurrencyData}
+        selectedCurrency={defaultSelectedCurrency}
+        onCurrencyChange={mockOnCurrencyChange}
+      />
+    );
+  });
+
+  it("renders the component with the correct label", () => {
+    expect(screen.getByLabelText(/Select Currency/i)).toBeInTheDocument();
+  });
+
+  it("should select the selected currency", async () => {
+    const textbox = screen.getByLabelText(/Select Currency/i);
+
+    expect(textbox).toHaveValue(defaultSelectedCurrency.toUpperCase());
+  });
+
+  it("should change the selected value when a different currency is selected", async () => {
+    const textbox = screen.getByLabelText(/Select Currency/i);
+
+    await userEvent.click(textbox);
+    const listBox = await screen.findByRole('listbox');
+    const usdOption = within(listBox).getByText('USD');
+    await userEvent.click(usdOption);
+
+    expect(textbox).toHaveValue('USD');
+    expect(mockOnCurrencyChange).toHaveBeenCalledExactlyOnceWith('usd');
+  });
+});
